ab66704bf146312c0695cf982dd3a8f317e363d0
fluorine21 <james.an.williams@gmail.com>
2020-11-21 18:03:39 -0800

    Finished add_channel, working on arm_board

--- python_source/add_channel.py
+++ python_source/add_channel.py
@@ -8,8 +8,8 @@ import rfsoc_board_driver as rbd
 #3 waveform period in nanoseconds, will be rounded to the nearest 4 nanoseconds
 #4 delay after experiment in nanoseconds, will be rounded to nearest 4 nanoseconds
 #5 waveform amplitude multiplication factor, must be between 0 and 1
-#6 number of cycles to playback the waveform
-#7 delay before experiment in nanoseconds, will be rounded to the nearest quarter nanosecond
+#6 number of cycles (times) to playback the waveform
+#7 delay before experiment in nanoseconds, will be rounded to the nearest quarter nanosecond (pre_delay)
 #8 is locking channel? Must be either 0 or 1
 #9 locking waveform amplitude factor, must be between 0 and 1
 #10 locking phase in nanoseconds, can be a float but will be rounded to nearest quarter nanosecond
@@ -33,8 +33,8 @@ waveform_period = int(sys.argv[3])#in nanoseconds, must be int
 post_delay = int(sys.argv[4]) #in nanoseconds, can be negative and a float
 amp_mul_factor = float(sys.argv[5]) #between 0 and 1
 
-num_cycles = int(sys.argv[6])
-if(num_cycles < 1):
+num_repeat_cycles = int(sys.argv[6])
+if(num_repeat_cycles < 1):
     raise ValueError("Error, number of cycles must be greater than 0")
 
 zero_delay = float(sys.argv[7]) #in nanoseconds, must be positive
@@ -54,8 +54,7 @@ locking_filename = ""
 if(is_locking):
     
     if(len(sys.argv) < 12):
-        print("Error, not enough arguments to parse locking channel.")
-        sys.exit()
+        raise ValueError("Error, not enough arguments to parse locking channel.")
 
     locking_amp_factor = float(sys.argv[9]) #between 0 and 1
     locking_phase = float(sys.argv[10]) #in nanoseconds, can be negative and a float
@@ -64,20 +63,66 @@ if(is_locking):
     locking_filename = sys.argv[11]
 
 
-board = ib.load_board()
 
-if(board == None):
-    print("Error, unable to load board.")
-    sys.exit()
-    
-#Calculate the number of repeat cycles based on the total number of cycles and the period
 #TODO
 
-if(board.add_channel(c)):
-    print("Error while adding channel")
+#Load the board object
+board = rbd.load_rfsoc_board()
 
-if(ib.save_board(board)):
-    print("Error while saving board to disk")
+#check and see if there is already a channel object for this channel
+found = 0
+for c in board.channel_list:
+    if(c.channel_num == channel_number and c.type == "DAC"):
+        found = 1
+        print("Appending configuration for channel " + str(channel_number))
+        #If we're about to overwrite something that is already configured
+        if(is_locking and c.locking_filename != ""):
+            print("Warning, overriding waveform configuration for channel " + str(channel_number))
+        if(!is_locking and c.waveform_filename != ""):
+            print("Warning, overriding locking cycle configuration for channel " + str(channel_number))
+        
+        if(is_locking):
+            c.locking_filename = locking_filename
+            c.locking_amp_factor = locking_amp_factor
+            c.locking_shift = locking_phase
+        else:
+            c.waveform_filename = waveform_filename
+            c.period = waveform_period
+            c.post_delay = post_delay
+            c.waveform_amp_factor = amp_mul_factor
+            c.num_repeat_cycles = num_repeat_cycles
+            c.pre_delay = zero_delay
+        
+        #Check the new parameters
+        c.check_parameters()
+        #Re-generate the channel data with the new values
+        c.generate_channel_data()
+        
+#If we just need to add a new channel
+if(found == 0):
+    print("Creating new configuration for channel " + str(channel_number))
+    c = rbd.rfsoc_channel(
+        "DAC", 
+        channel_number, 
+        locking_phase, 
+        locking_amp_factor, 
+        zero_delay,
+        post_delay,
+        waveform_period,
+        num_repeat_cycles,
+        waveform_amp_factor,
+        waveform_filename,
+        locking_filename,
+        0, 0)
+    
+    board.channel_list.append(c)
+
+#Save the board object to disk
+rbd.save_rfsoc_board(board)
+
+#Make sure that last step worked
+if(rbd.load_rfsoc_board() != board):
+    raise ValueError("Error, unable to save board object state to disk")
 else:
     print("Successfully added channel #" + str(channel_number+1))
     print("Waveform file: " + waveform_filename + 
@@ -89,5 +134,5 @@ else:
           " (ns)\nIs locking channel: " + ("YES" if is_locking == 1 else "NO") + 
           "\nLocking file: " + locking_filename + 
           "\nLocking amplitude factor: " + str(locking_amp_factor) + "\nLocking phase: " + str(locking_phase) + " (ns)" + 
-          "\nPre-waveform fix (subtract 4ns from total period): " + ("YES" if pre_waveform_fix == 1 else "NO") + 
-          "\nOne long waveform: " + ("YES" if use_loopback == 0 else "NO") + "\n")
+          "\nPre-waveform fix (subtract 4ns from total period): " + "IGNORED" + 
+          "\nOne long waveform: " + "IGNORED" + "\n")
---
+++ python_source/arm_board.py
@@ -0,0 +1,20 @@
+
+
+import rfsoc_board_driver as rbd
+
+
+#First argument is nanoseconds to run ADC for
+#second argument is 1 for display uploaded waveforms
+#third argument is number of adc averages
+
+
+#Load the board state
+
+#Add an ADC channel (or append the current one)
+
+#Check the status of the clocks
+
+#Flush all the buffers and disable adc readout
+
+#Configure all channels
+
